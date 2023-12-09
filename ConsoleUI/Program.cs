58bfd42303c75d85650321c0a3333efefa03a3f9
Aslan Məmmədov <aslan.mammadov.m@asoiu.edu.az>
2023-12-09 17:01:52 +0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.Add(new Car { Id=5,Description = "Ela" });
+            carManager.Update(new Car {Id=1,BrandId=7,ColorId=7,DailyPrice=77777,ModelYear=1977,Description="7777777" });
+            foreach (Car item in carManager.GetAll())
+            {
+                Console.WriteLine($"{item.Id} - {item.BrandId} - {item.ColorId} - {item.ModelYear} - {item.DailyPrice} - {item.Description}");
+            }
+            Console.WriteLine("\n---------------------------------------------\n");
+            carManager.Delete(new Car { Id = 5});
+            foreach (Car item in carManager.GetAll())
+            {
+                Console.WriteLine($"{item.Id} - {item.BrandId} - {item.ColorId} - {item.ModelYear} - {item.DailyPrice} - {item.Description}");
+            }
+            Console.WriteLine("\n---------------------------------------------\n");
+            Console.WriteLine($"{carManager.GetById(4).Id} - {carManager.GetById(4).BrandId} - {carManager.GetById(4).ColorId} - {carManager.GetById(4).ModelYear} - {carManager.GetById(4).DailyPrice} - {carManager.GetById(4).Description}");
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=1998,DailyPrice=11500,Description="Orta....." },
+                new Car{Id=2,BrandId=2,ColorId=2,ModelYear=2005,DailyPrice=16000,Description="Yaxsi....." },
+                new Car{Id=3,BrandId=3,ColorId=3,ModelYear=2010,DailyPrice=21567,Description="Ideal....." },
+                new Car{Id=4,BrandId=4,ColorId=4,ModelYear=2015,DailyPrice=25712,Description="Defekti var....." }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
